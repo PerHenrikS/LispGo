500f3e774326580cee8f992f6d911d1a21afe462
Henrik Söderberg <henkkasoderberg@hotmail.com>
2018-08-28 12:51:47 +0300

    Test file comments

--- test.clj
+++ test.clj
@@ -1,8 +1,8 @@
-;This is a comment 
-(defn somenumber 29)
+;Using clojure as a test file for syntax highlight in vscode
+(defn somenumber 29)            ;define variable somenumber 
 
-(defun incr (x) (+ x 1))
+(defun incr (x) (+ x 1))        ;Increments input 
 
-(print (incr somenumber))
+(print (incr somenumber))       ;Prints 30
 
-(print (tail (list 1 2 3 4)))
+(print (tail (list 1 2 3 4)))   ;Prints [2 3 4]
