b8435c9d8905d81f8fee203b31b8abc279940cb2
Henrik Söderberg <henkkasoderberg@hotmail.com>
2018-08-28 13:06:48 +0300

    Better help in CLI

--- main.go
+++ main.go
@@ -13,11 +13,15 @@ import (
 )
 
 func main() {
+	helpText := `
+	Usage: 
+		alisp <sourcefile> 		- Interpretes a source file 
+		alisp repl			- Starts repl
+	`
+
 	if len(os.Args) == 2 {
 		arg := os.Args[1]
 		switch arg {
-		case "help":
-			fmt.Println("Show help")
 		case "repl":
 			reader := bufio.NewReader(os.Stdin)
 			repl.Start(reader)
@@ -39,6 +43,6 @@ func main() {
 			}
 		}
 	} else {
-		fmt.Println("Print usage")
+		fmt.Println(helpText)
 	}
 }
